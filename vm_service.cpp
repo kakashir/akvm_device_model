874ec61a6257b64f7f5746ad249678d8cc45dbad
Yuan Yao <yaoyuan0329os@gmail.com>
2024-04-20 21:50:26 +0800

    Vm Service: Introduce IO vm service
    Signed-off-by: Yuan Yao <yaoyuan0329os@gmail.com>

--- vm_service.cpp
+++ vm_service.cpp
@@ -4,14 +4,20 @@
 #include "vm_service.hpp"
 #include "device_hub.hpp"
 
-#define VM_SERVICE_TYPE_DEBUG 0xfeULL
-#define	VM_SERVICE_TYPE_PANIC 0xffULL
+#define VM_SERVICE_DEBUG 0xfeULL
+#define VM_SERVICE_PANIC 0xffULL
+
+#define VM_SERVICE_IO 0x1
+#define VM_SERVICE_IO_PORT_IN  0x1
+#define VM_SERVICE_IO_PORT_OUT 0x2
+#define VM_SERVICE_IO_MMIO_IN 0x3
+#define VM_SERVICE_IO_MMIO_OUT 0x4
 
 static int handle_vm_service_debug_panic(u64 type,
 					 u64 *in_out, u64 in_out_count)
 {
 	printf("vcpu %s:\n",
-	       type == VM_SERVICE_TYPE_DEBUG ? "DEBUG" : "PANIC");
+	       type == VM_SERVICE_DEBUG ? "DEBUG" : "PANIC");
 	printf("  arg0: 0x%lx\n", in_out[0]);
 	printf("  arg1: 0x%lx\n", in_out[1]);
 	printf("  arg2: 0x%lx\n", in_out[2]);
@@ -19,14 +25,39 @@ static int handle_vm_service_debug_panic(u64 type,
 	printf("  arg4: 0x%lx\n", in_out[4]);
 	printf("  arg5: 0x%lx\n", in_out[5]);
 
-	return type == VM_SERVICE_TYPE_DEBUG ? 0 : 1;
+	return type == VM_SERVICE_DEBUG ? 0 : 1;
+}
+
+static int handle_vm_service_io(u64 *in_out, u64 in_out_count)
+{
+	DeviceHub &device_hub = DeviceHub::instance();
+	u64 type = in_out[0];
+	gpa addr = in_out[1];
+	u64 size = in_out[2];
+
+	switch(type) {
+	case VM_SERVICE_IO_PORT_IN:
+		return device_hub.handle_port_read(addr, size, in_out[3]);
+	case VM_SERVICE_IO_PORT_OUT:
+		return device_hub.handle_port_write(addr, size, in_out[3]);
+	case VM_SERVICE_IO_MMIO_IN:
+		return device_hub.handle_mmio_read(addr, size, in_out[3]);
+	case VM_SERVICE_IO_MMIO_OUT:
+		return device_hub.handle_mmio_write(addr, size, in_out[3]);
+	}
+
+	printf("IO failed: addr:0x%lx type:0x%x size:0x%llx val:0x%llx\n",
+	       addr, type, size, in_out[3]);
+	return -ENOTSUP;
 }
 
 int handle_vm_service(u64 type, u64 *in_out, u64 in_out_count)
 {
 	switch(type) {
-	case VM_SERVICE_TYPE_DEBUG:
-	case VM_SERVICE_TYPE_PANIC:
+	case VM_SERVICE_IO:
+		return handle_vm_service_io(in_out, in_out_count);
+	case VM_SERVICE_DEBUG:
+	case VM_SERVICE_PANIC:
 		return handle_vm_service_debug_panic(type, in_out, in_out_count);
 	default:
 		printf("unsupported vm service type: %d\n", type);
